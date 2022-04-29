4f9ac39bb84569fa2cf82fda437ad42da281b345
walteronoh <walterkiprono81@gmail.com>
2022-04-30 00:46:15 +0300

    feat: add function to get and alert currently clicked string

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,11 @@ import { Book_Contents } from "../resources/books.resources";
 
 function Home() {
     const { loading, data } = useQuery(Book_Contents);
-    const [ currentPage, setCurrentPage ] = useState(0);
+    const [currentPage, setCurrentPage] = useState(0);
+    if (!loading) {
+        console.log(data.book.pages[1].content[2], '\n');
+        console.log(data.book.pages[1]);
+    }
 
     // Open next page function
     const goToNextPage = () => {
@@ -14,7 +18,20 @@ function Home() {
 
     // Open previous page function
     const goToPreviousPage = () => {
-        if(currentPage > 0) setCurrentPage(currentPage - 1);
+        if (currentPage > 0) setCurrentPage(currentPage - 1);
+    }
+
+    // Get String value
+    const getStringValue = (page: any) => {
+        // Return a Selection Object representing the range of text selected by the user.
+        let s = window.getSelection();
+        // Return a Range Object representing one of the range currently selected.
+        let range = s?.getRangeAt(0);
+        // Return a number representing where in the start container the range starts
+        let startOffset = range?.startOffset;
+        // Filter tokens using the startOffset
+        let strArr = data.book.pages[page].tokens.filter((token: any) => (token.position[0] <= startOffset!) && (token.position[1] >= startOffset!));
+        alert(strArr[0].value)
     }
 
     if (loading) return <div> Loading! </div>
@@ -26,12 +43,12 @@ function Home() {
             </header>
             <main>
                 <div className="leftPage">
-                    <pre>{data.book.pages[currentPage].content}</pre>
+                    <pre onClick={() => getStringValue(currentPage)}>{data.book.pages[currentPage].content}</pre>
                     <button onClick={goToPreviousPage} className="leftPageBtn">Left</button>
                     <div className="leftPageNo">1</div>
                 </div>
                 <div className="rightPage">
-                    <pre>{data.book.pages[currentPage + 1].content}</pre>
+                    <pre onClick={() => getStringValue(currentPage + 1)}>{data.book.pages[currentPage + 1].content}</pre>
                     <button onClick={goToNextPage} className="rightPageBtn">Right</button>
                     <div className="rightPageNo">2</div>
                 </div>
